df65f1d4e075105be55a6a645203c04efa6e1c4b
Lucian Lodico <llodico@mac.com>
2023-01-30 14:02:42 -0500

    questions, no answer ticker yet

--- trivial-guess/src/App.js
+++ trivial-guess/src/App.js
@@ -1,87 +1,92 @@
-import './App.css';
-import { useState, useEffect } from 'react'; 
-import axios from 'axios'; 
-import { he } from 'he'; // might use this later 
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import he from "he";
 
 function App() {
   return (
-    <div>
-      <Cats /> 
-    </div>
-  )
-} 
-
-
-function Cats() {
-  const [categories, setCategories] = useState([]); 
-
-  useEffect(() => {
-    axios 
-    .get('https://opentdb.com/api_category.php')
-    .then((response) => {
-      setCategories(response.data.trivia_categories);
-  }); 
-}, []) 
-
-const [selectedCategory, setSelectedCategory] = useState("") 
-
-if (selectedCategory) {
-
-  console.log(selectedCategory)
-
-  return <Quest /> 
-}
-
-  return (
-      <div>
-        <h1>Trivial Guess</h1>
-        <div className="Cats-the-musical"> 
-          { categories.map(categories => (
-              <button className="boop" onClick={() => setSelectedCategory(categories.id)} key={categories.id}><li>{categories.name}</li></button>
-            ))}
-        </div> 
-      </div> 
-  )
-}
-
-
- function Quest(selectedCategory) {
-  const [question, setQuestion] = useState([])
-
-      useEffect(() => {
-      axios 
-      .get('https://opentdb.com/api.php?amount=4')
-      .then((response) =>{setQuestion(response.data.results.map(obj => [obj.question, obj.incorrect_answers, obj.correct_answer]))})
-    }, [selectedCategory])
-
-  
-  return (
     <>
-     {question && question.map((q) =>
-
-(
-<div>
-<h1 className="qtitle" key={selectedCategory}>{q[0].replace('&quot;','"').replace('&quot;','"').replace('&#039;', "'")}</h1>
-
-<ul className='questions'>
-<li className='category'><button>{q[1][0]}</button></li>
-<br/> 
-<li className='category'><button>{q[1][1]}</button></li>
-<br/> 
-<li className='category'><button>{q[1][2]}</button></li>
-<br/>
-<li className='category'><button>{q[2]}</button></li>
-</ul>
-
-</div>)
-
-)}
+      <header>
+        <h1 className="title">Trivial Guess</h1>
+      </header>
+      <main>
+        <div className="App">
+            <Category />
+        </div>
+      </main>
     </>
-  )
+  );
+}
 
- }
+function Category({ id, name }) {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api_category.php")
+      .then((res) =>
+        setCategories(
+          res.data.trivia_categories.map((obj) => [obj.id, obj.name])
+        )
+      );
+  }, []);
 
-  
+  const [selectedCategory, setSelectedCategory] = useState("");
+  if (selectedCategory) {
+    return <CatQuestion selectedCategory={selectedCategory} />;
+  }
 
+  return (
+    <div>
+        {categories.map(([catId, catName]) => (
+          <div className="category" onClick={() => setSelectedCategory(catId)} key={catId}>
+            <button className="silly">{catName}</button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function CatQuestion({ selectedCategory }) {
+  const [question, setQuestion] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  useEffect(() => {
+    axios
+      .get(`https://opentdb.com/api.php?amount=1&category=${selectedCategory}`)
+      .then((res) => {
+        setQuestion(
+          res.data.results.map((obj) => ({
+            question: he.decode(obj.question),
+            correctAnswer: obj.correct_answer,
+            answerChoices: [obj.correct_answer, ...obj.incorrect_answers],
+          }))
+        );
+      });
+  }, [selectedCategory]);
+
+  if (answer) {
+    if (answer === question.correctAnswer) {
+      setScore(score + 1);
+    }
+    return <CatQuestion selectedCategory={selectedCategory} setScore={setScore} />;
+  }
+
+  return (
+    <div className="question">
+      {question.map((quest) => (
+        <div key={selectedCategory}>
+          <h1>{quest.question}</h1>
+          <ul className="answers" key={quest}>
+            {quest.answerChoices.map((a) => (
+              <div className="answerz" onClick={() => setAnswer(he.decode(a))} key={a}>
+                <button className="bop">{he.decode(a)}</button>
+              </div>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
